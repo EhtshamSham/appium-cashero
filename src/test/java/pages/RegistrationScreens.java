e99da0663b7bb140228858778bc70e5bf7d2d311
Muhammad Ehtsham <ehtsham@rnssol.com>
2021-08-09 17:00:35 +0500

    removed unused libraries

--- src/test/java/pages/RegistrationScreens.java
+++ src/test/java/pages/RegistrationScreens.java
@@ -5,9 +5,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.pagefactory.AndroidFindBy;
 
 public class RegistrationScreens extends AbstractPageObject{
 
